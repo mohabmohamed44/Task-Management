bc990c3a9482fd18d16fecf71d86c6a9c62f2b0c
mohabmohamed44 <mohamedmohab742@gmail.com>
2026-02-13 17:36:59 +0200

    Remove HydrationBoundary and dehydratedState from App component becuase SSR only works on Next.js, Remix, Nuxt

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,23 @@ import { RouterProvider } from 'react-router'
 import { appRouter } from "./app/router.tsx";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
-import { QueryClient, QueryClientProvider, HydrationBoundary, type DehydratedState } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ModalProvider } from './app/Providers/ModalProvider'
 import './App.css'
 
 const queryClient = new QueryClient();
 
-function App({ dehydratedState }: { dehydratedState: DehydratedState }) {
+function App() {
   return (
     <QueryClientProvider client={queryClient}> 
-      <HydrationBoundary state={dehydratedState}>
-        <ModalProvider>
-          <ReactQueryDevtools initialIsOpen={false} buttonPosition='bottom-left' />
-          <RouterProvider router={appRouter} />
-          <Toaster 
-            position='top-center'
-            reverseOrder={false}
-          />
-        </ModalProvider>
-      </HydrationBoundary>
+      <ModalProvider>
+        <ReactQueryDevtools initialIsOpen={false} buttonPosition='bottom-left' />
+        <RouterProvider router={appRouter} />
+        <Toaster 
+          position='top-center'
+          reverseOrder={false}
+        />
+      </ModalProvider>
     </QueryClientProvider>
   )
 }
